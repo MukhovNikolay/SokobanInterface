0b92a678e4ad09a6ac4a87d97aaab5214206c936
Николай Мухов <75414376+MukhovNikolay@users.noreply.github.com>
2023-04-24 13:54:35 +0300

    Add files via upload

---
+++ SokobanInterface/app/src/main/java/com/example/sokobaninterface/Character.kt
@@ -0,0 +1,6 @@
+package com.example.sokobaninterface
+
+import android.graphics.Bitmap
+
+class Character(image: Bitmap, x: Int, y: Int) :GameObject(image, x, y) {
+}
---
+++ SokobanInterface/app/src/main/java/com/example/sokobaninterface/GameObject.kt
@@ -0,0 +1,11 @@
+package com.example.sokobaninterface
+
+import android.graphics.Bitmap
+
+abstract class GameObject(val image: Bitmap,val x:Int,val y:Int ) {
+    val width = image.width
+    val height = image.height
+
+
+//    GameObject()
+}
